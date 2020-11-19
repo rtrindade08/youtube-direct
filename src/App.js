d23f22c47bb3369b8db30319f66f8f09c9e532fb
rtrindade08 <renata.trindade@kufuta.co>
2020-11-19 22:36:51 +0000

    started the searchbar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
 import "./App.css";
-
+import SearchBar from "./search-bar";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SearchBar />
     </div>
   );
 }
