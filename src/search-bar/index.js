b939da53e0e942dfd0b8f11b034080a41f1388d7
renata <renata.trindade@kufuta.co>
2020-12-03 22:29:34 +0000

    created search bar component and added some fixes

--- src/search-bar/index.js
+++ src/search-bar/index.js
@@ -1,5 +1,11 @@
 import React from "react";
-
-export default () => {
-  return <input placeholder="teste"></input>;
+import "./styles.scss";
+const SearchBar = () => {
+  return (
+    <div>
+      <input placeholder={"teste"} className="searchBar" />
+    </div>
+  );
 };
+
+export default SearchBar;
