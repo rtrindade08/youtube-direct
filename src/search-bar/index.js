b263367e631cbe5440c3488b1cd36588f461bb2d
rtrindade <renata.trindade@kufuta.co>
2020-12-05 14:29:05 +0000

    minor fixes

--- src/search-bar/index.js
+++ src/search-bar/index.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.scss";
 const SearchBar = () => {
+  const [keyword, setKeyword] = useState("");
+
   return (
     <div>
-      <input placeholder={"teste"} className="searchBar" />
+      <input
+        placeholder={"teste"}
+        className="searchBar"
+        value={keyword}
+        onChange={(e) => setKeyword(e.target.value)}
+      />
     </div>
   );
 };
