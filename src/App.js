70c87308e4b6463a259d54fafc7e6692eb77af63
rtrindade <renata.trindade@kufuta.co>
2020-12-05 21:20:33 +0000

    app done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
-import "./App.css";
-import SearchBar from "./search-bar";
+import React, { useState } from "react";
+import SearchBar from "./components/search-bar";
+import VideoDetails from "./components/video-details";
+import VideoList from "./components/video-list";
+
+import youtube from "../src/api/youtube";
+
+import { Grid } from "@material-ui/core";
 function App() {
+  const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
   return (
-    <div className="App">
-      <SearchBar />
-    </div>
+    <Grid style={{ justifyContent: "center" }} container spacing={10}>
+      <Grid item xs={11}>
+        <Grid container spacing={10}>
+          <Grid item xs={12}>
+            <SearchBar onSubmit={handleSubmit} />
+          </Grid>
+          <Grid item xs={8}>
+            <VideoDetails video={selectedVideo} />
+          </Grid>
+          <Grid item xs={4}>
+            <VideoList videos={videos} onVideoSelect={setSelectedVideo} />
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
   );
+  async function handleSubmit(searchTerm) {
+    const {
+      data: { items: videos },
+    } = await youtube.get("search", {
+      params: {
+        part: "snippet",
+        maxResults: 5,
+        key: "AIzaSyCNWGrE-uNAe5bB83YVZ9Bia5XQpzkJnWk",
+        q: searchTerm,
+      },
+    });
+
+    setVideos(videos);
+    setSelectedVideo(videos[0]);
+  }
 }
 
 export default App;
