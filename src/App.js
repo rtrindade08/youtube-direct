93eae49f825d1061d11b0e81eb12c04647c7c88e
rtrindade <renata.trindade@kufuta.co>
2020-12-06 17:41:34 +0000

    fixes

--- src/App.js
+++ src/App.js
@@ -2,19 +2,25 @@ import React, { useState } from "react";
 import SearchBar from "./components/search-bar";
 import VideoDetails from "./components/video-details";
 import VideoList from "./components/video-list";
-
 import youtube from "../src/api/youtube";
 
 import { Grid } from "@material-ui/core";
 function App() {
   const [videos, setVideos] = useState([]);
   const [selectedVideo, setSelectedVideo] = useState(null);
+
+  const handleVideos = async (searchTerm) => {
+    const result = await youtube.get("/search", { params: { q: searchTerm } });
+    setVideos(result.data.items);
+    setSelectedVideo(result.data.items[0]);
+  };
+
   return (
     <Grid style={{ justifyContent: "center" }} container spacing={10}>
       <Grid item xs={11}>
         <Grid container spacing={10}>
           <Grid item xs={12}>
-            <SearchBar onSubmit={handleSubmit} />
+            <SearchBar onSubmit={handleVideos} />
           </Grid>
           <Grid item xs={8}>
             <VideoDetails video={selectedVideo} />
@@ -26,21 +32,6 @@ function App() {
       </Grid>
     </Grid>
   );
-  async function handleSubmit(searchTerm) {
-    const {
-      data: { items: videos },
-    } = await youtube.get("search", {
-      params: {
-        part: "snippet",
-        maxResults: 5,
-        key: "AIzaSyCNWGrE-uNAe5bB83YVZ9Bia5XQpzkJnWk",
-        q: searchTerm,
-      },
-    });
-
-    setVideos(videos);
-    setSelectedVideo(videos[0]);
-  }
 }
 
 export default App;
--- src/api/youtube.js
+++ src/api/youtube.js
@@ -2,4 +2,9 @@ import axios from "axios";
 
 export default axios.create({
   baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    maxResults: 5,
+    key: "AIzaSyCNWGrE-uNAe5bB83YVZ9Bia5XQpzkJnWk",
+  },
 });
